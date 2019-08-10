ebd20e3014e50d5fb53ee2edf46a628eb0d7c4fd
tone.chromebook <tonetheman@gmail.com>
2019-08-10 14:59:32 -0500

    calling routine to download to take care of python version diffs

--- tiktok-dl.py
+++ tiktok-dl.py
@@ -5,7 +5,7 @@ VERBOSE = False
 import sys
 VERSION = sys.version_info[0]
 
-def dowload_data(uri):
+def download_data(uri):
     UA_CHROME = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
     r1 = None
     class Junk:
@@ -46,23 +46,7 @@ def mainline():
     else:
         import urllib3
 
-    # this is chrome76
-    UA_CHROME = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
-    user_agent = {'user-agent': UA_CHROME }
-    if VERSION==2:
-        import urllib2
-        req = urllib2.Request(args.uri, headers={ 'User-Agent': UA_CHROME })
-        class Junk:
-            def __init__(self):
-                self.data = None
-        r1 = Junk()
-        r1.data = urllib2.urlopen(req).read()
-    else:
-        # 3
-        http = urllib3.PoolManager(10, headers=user_agent)
-
-        print("getting url data...")
-        r1 = http.urlopen('GET', args.uri)
+    r1 = download_data(args.uri)
     
     if args.debug and args.verbose:
         print(r1.data)
@@ -89,17 +73,9 @@ def mainline():
     else:
         print("did not find a video link :(", len(links))
 
-        
-    if VERSION==2:
-
-        req = urllib2.Request(links[0], headers={ 'User-Agent': UA_CHROME })
-        r2 = Junk()
-        r2.data = urllib2.urlopen(req).read()
-
-
-    else:
-        r2 = http.urlopen('GET', links[0])
-
+    
+    r2 = download_data(links[0])
+ 
     outf = open(args.output,"wb")
     outf.write(r2.data)
     outf.close()
